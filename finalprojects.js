7c7aba835cf8ca1c025cb6e4a9b3a5485b0736f7
Aaron McCollum <59707347+aaronmccollum@users.noreply.github.com>
2021-11-23 19:26:28 +0900

    Answered the third challenge problem

--- finalprojects.js
+++ finalprojects.js
@@ -19,3 +19,40 @@ const palindrome = (str) => {
   }
   return false;
 }
+
+// ----------------------------------- //
+
+// 3. Caesars Cipher
+
+// Function that shifts a given letter by a given number in the alphabet.
+const shiftLetter = (letter, num) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmn";
+  if (alphabet.includes(letter)) {
+    let index = alphabet.indexOf(letter);
+    let shift = index + num;
+    return alphabet[shift];
+  };
+  return letter;
+};
+
+// A function that loops through the given string to create a shiftedLetters array.
+// Then it loops again to replace undefinded values with a space.
+// Lastly it returns a joined array in all upper case to pass the tests.
+const rot13 = (str) => {
+  let lowerCaseStr = str.toLowerCase();
+  let lowerLetters = lowerCaseStr.split("");
+  let shiftedLetters = [];
+
+  for (let i = 0; i < lowerLetters.length; i++) {
+    let letter = lowerLetters[i];
+    shiftedLetters.push(shiftLetter(letter, 13));
+  }
+
+  for (let j = 0; j < shiftedLetters.length; j++) {
+    if (shiftedLetters[j] === undefined) {
+      shiftedLetters[j] = " ";
+    }
+  }
+
+  return shiftedLetters.join("").toUpperCase();
+}
